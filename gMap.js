6be660cb06ea6c575d03fcf17a84beeeb0488cbd
JaysonChiang <jayson.chiang@outlook.com>
2014-12-14 00:33:37 +0800

    multiple busRoute plotting

--- gMap.js
+++ gMap.js
@@ -1,5 +1,13 @@
 $(document).ready(function(){
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 /***for NTP
 
@@ -49,8 +57,14 @@ google.maps.event.addDomListener(window, "load", initialize);
            // marker.setPosition( new google.maps.LatLng( 24, 122 ) );
            // map.panTo( new google.maps.LatLng( 24, 122 ) );
 
+/*
             $.getJSON( "data/bus-tp-stop.json", function( data ) {
                 
+               
+                var myStation = data.features.filter(function(s){
+                    return s.properties.bsm_chines === "";     
+                });
+
                 data.features.forEach(function(item){
                 
                     var position = new google.maps.LatLng( 
@@ -63,17 +77,25 @@ google.maps.event.addDomListener(window, "load", initialize);
                         title: item.properties.bsm_chines
                     });
                 });
+ 
             });
-
+*/
+/***
+ ["111","1717"]
+*/
+// var station_SL = ["111","1717","206","255","255區","260","260區","303","303區",
+// "304","304承德","304重慶","557","620","680","683",
+// "內科通勤專車13","內科通勤專車15","內科通勤專車16","小15","小15區",
+// "小16","小17","小18","小18區","小19","市民小巴1","紅30","紅5","重慶幹線"];
+var station_SL = ["111","1717","206","255","260","303","304","557","620","680","683"];
 
             $.getJSON( "data/bus-tp-route.json", function( data ) {
-                
-                var myRoute = data.features.filter(function(s){
-                    return (s.properties.bad_chines === "645") ;
-                });
-                console.log(myRoute);
-                
-                myRoute.forEach(function(item){
+
+                var myRoute = data.features
+                .filter(function(s){
+                    return station_SL.indexOf(s.properties.bad_chines) > 0 ;
+                })
+                .forEach(function(item){
 
                     var flightPlanCoordinates = item.geometry.coordinates.map(function(point){
                         return (new google.maps.LatLng(point[1], point[0]));
@@ -82,9 +104,9 @@ google.maps.event.addDomListener(window, "load", initialize);
                     var flightPath = new google.maps.Polyline({
                         path: flightPlanCoordinates,
                         geodesic: true,
-                        strokeColor: '#FF0000',
+                        strokeColor: getRandomColor(), //#FF0000
                         strokeOpacity: 1.0,
-                        strokeWeight: 2
+                        strokeWeight: 4
                     });
 
                     flightPath.setMap(map);
